bb9feb6c60945517d8fb1d9fb23bdf459627abc3
Joseph A <ahnjoseph654@gmail.com>
2022-04-03 21:18:23 -0400

    Started with and worked on step 1 of 5 on 4/03/22.

---
+++ PHP-League/index.php
@@ -0,0 +1,74 @@
+<DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title> HTML template </title>
+
+    <!-- Brief description of program -->
+    <meta name="description" content="This program is made to display sample data">
+
+  </head>
+
+  <body>
+
+    <main role="main">
+
+      <table>
+        <caption> Upcoming Games in the ROC Venue </caption>
+          <tr>
+             <th> Date </th>
+             <th> Time </th>
+             <th> Visitor @ Home </th>
+             <th> Score </th>
+             <th> Venue </th>
+          </tr>
+          <tr>
+            <td> </td>
+          </tr>
+      </table>
+
+      <br /> <br />
+
+      <table>
+        <caption> Upcoming Schedule for the Nightshawk Team </caption>
+          <tr>
+             <th> Date </th>
+             <th> Time </th>
+             <th> Opponent </th>
+             <th> Venue </th>
+             <th> Status </th>
+             <th> Score </th>
+          </tr>
+          <tr>
+            <td> </td>
+          </tr>
+      </table>
+
+      <br /> <br />
+
+      <table>
+        <caption> Current Standings for the Intermediate Boys Division </caption>
+          <tr>
+             <th> Place </th>
+             <th> Team </th>
+             <th> Won </th>
+             <th> Lost </th>
+             <th> Tied </th>
+             <th> Pct </th>
+             <th> PF </th>
+             <th> PA </th>
+          </tr>
+          <tr>
+            <td> </td>
+          </tr>
+      </table>
+
+    </main>
+
+  </body>
+
+</html>

dadf954892b15d5fd6bfce4214ebefe1e3f21913
Joseph A <ahnjoseph654@gmail.com>
2022-04-06 00:34:21 -0400

    Saving all edits made to Venue.php

---
+++ PHP-League/class/Venue.php
@@ -0,0 +1,19 @@
+<!--
+Work log for Venue.php
+Created on 4/05/22
+4/05/22 set up Venue class
+-->
+
+<?php
+
+  Class Venue {
+
+    //name (string)
+    $venue_name = "";
+
+    //teams (Array of Team objecs in this division)
+    $teams_in_venue = array();
+
+  }
+
+?>

0ce3cad80d854299186002267bdb94017fe11a73
Joseph A <ahnjoseph654@gmail.com>
2022-04-06 00:32:26 -0400

    Saving any changes to Game.php

---
+++ PHP-League/class/Game.php
@@ -0,0 +1,44 @@
+<!--
+Work log for Game.php
+Created on 4/05/22
+4/05/22 Started to set up and work on Game class
+-->
+
+<?php
+
+  class Game {
+
+    //datetime (datetime object)
+    public DateTime::format(String$format): string;
+
+
+    //home_team (team object)
+    public $home_team;
+
+    $home = new home_team;
+
+
+    //home_score (integer)
+    $home_score;
+
+    settype($home_Score, "integer");
+
+
+    //visitor_team (team object)
+    public $visitor_team;
+
+    $visitor = new visitor_team;
+
+
+    //visitor_score (integer)
+    $visitor_score;
+
+
+    //venue (venue object)
+    public $venue;
+
+
+  }
+
+
+?>

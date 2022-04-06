09ad4693e3fa760086b2ca1e8dac3f4b2aa5a7b8
Joseph A <ahnjoseph654@gmail.com>
2022-04-06 00:34:00 -0400

    Saving all changes made to Division.php

---
+++ PHP-League/class/Division.php
@@ -0,0 +1,23 @@
+<!--
+Work log for Division.php
+Created on 4/05/22
+4/05/22 set up Division class
+-->
+
+<?php
+
+  Class Division {
+
+    //name (String)
+    $division_name = "";
+
+
+    //teams (arrays of Team objects in this division)
+    $division_teams = array();
+
+
+
+
+  }
+
+?>

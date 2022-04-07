96b16d91c8e3c01f697a3283386792af97630918
Joseph A <ahnjoseph654@gmail.com>
2022-04-07 14:47:51 -0400

    created schedule template

--- PHP-League/index.php
+++ PHP-League/index.php
@@ -22,71 +22,9 @@ Work log for index.php
 
     <main role="main">
 
-      <table>
-        <caption> Upcoming Games in the ROC Venue </caption>
-          <tr>
-             <th> Date </th>
-             <th> Time </th>
-             <th> Visitor @ Home </th>
-             <th> Score </th>
-             <th> Venue </th>
-          </tr>
-          <tr>
-            <td> Fri Feb 11 </td>
-            <td> 5PM </td>
-            <td> Golden Griffins @ Nighthawks </td>
-            <td> 6-32 </td>
-            <td> ROC </td>
-          </tr>
-      </table>
-
-      <br /> <br />
-
-      <table>
-        <caption> Upcoming Schedule for the Nightshawk Team </caption>
-          <tr>
-             <th> Date </th>
-             <th> Time </th>
-             <th> Opponent </th>
-             <th> Venue </th>
-             <th> Status </th>
-             <th> Score </th>
-          </tr>
-          <tr>
-            <td> Sat Jan 15 </td>
-            <td> 5pm </td>
-            <td> Greyhounds </td>
-            <td> ROC </td>
-            <td> T </td>
-            <td> 12-12 </td>
-          </tr>
-      </table>
-
-      <br /> <br />
-
-      <table>
-        <caption> Current Standings for the Intermediate Boys Division </caption>
-          <tr>
-             <th> Place </th>
-             <th> Team </th>
-             <th> Won </th>
-             <th> Lost </th>
-             <th> Tied </th>
-             <th> Pct </th>
-             <th> PF </th>
-             <th> PA </th>
-          </tr>
-          <tr>
-            <td> #1 </td>
-            <td> Nighthawks </td>
-            <td> 8 </td>
-            <td> 1 </td>
-            <td> 0 </td>
-            <td> .889 </td>
-            <td> 199 </td>
-            <td> 132 </td>
-          </tr>
-      </table>
+      <?php
+        require("template/schedule.php");
+      ?>
 
     </main>
 

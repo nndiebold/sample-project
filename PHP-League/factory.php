7b12e7da2681961af15e42f4831a26f42c0b86fd
Joseph A <ahnjoseph654@gmail.com>
2022-04-07 14:09:30 -0400

    instantiated sample data

--- PHP-League/factory.php
+++ PHP-League/factory.php
@@ -8,6 +8,21 @@ Created on 4/05/22
 
   spl_autoload_register();
 
+  $division = new Division("Senior Boys", array());
 
+  $venue = new Venue("ROC", array());
 
+  $team1_stats = new Stats(4,3,0,4/7,150,150,(4*2)+(3*0)+(0*1));
+
+  $team1 = new Team("Hawks", array() ,$team1_stats, $division);
+
+  $team2_stats = new Stats(1,5,1,1/5,150,120,(1*2)+(5*0)+(1*1));
+
+  $team2 = new Team("Pigeons", array(), $team2_stats, $division);
+
+  $date_time = new DateTime("2022/1/31 5pm");
+
+  $game = new Game($date_time, $team1, 3, $team2, 2, $venue);
+
+  print_r($game);
 ?>
